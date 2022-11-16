b2783a370f57440ddb1f289ccf09f00fc9174a09
Gavin Tersteeg <gctersteeg@gmail.com>
2022-11-15 21:23:37 -0600

    Started work on the image upload endpoint

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import fileUpload from 'express-fileupload';
 import path from 'path';
 import url from 'url';
 
@@ -7,8 +8,12 @@ const port = process.env.PORT;
 
 // *** EXPRESS URL RESOLUTION STUFF ***
 
-app.get('/*', (req, res) => {
+app.use(fileUpload());
 
+/**
+ * We handle HTTP get requests here
+ */
+app.get('/*', (req, res) => {
   // Default page
   if (req.path == '/') {
     fsend(res, 'html/home.html');
@@ -23,6 +28,36 @@ app.get('/*', (req, res) => {
   else res.send('Invalid path ' + req.path + '!');
 });
 
+/**
+ * We handle HTTP post requests here
+ */
+app.post('/*', (req, res) => {
+
+  // General image upload portal here
+  if(req.path == '/upload') {
+    // Grab an image (if it exists) out of the attached file
+    const { image } = req.files;
+    
+    // Check to see if it exists
+    if (!image) {
+      console.log("Empty upload detected!");
+      return res.sendStatus(400);
+    }
+
+    // Make sure it is actually an image
+    if (!/image/.test(image.mimetype)) {
+      console.log('Non-image upload detected!');
+      console.log('Image mimetype: ' + image.mimetype);
+      return res.sendStatus(400);
+    }
+
+    console.log("Recieved an image");
+
+    // Send all good status
+    res.sendStatus(200);
+  }
+});
+
 app.listen(port, () => {
   console.log(`We are up and running, on port ${port}`);
 });
@@ -33,7 +68,7 @@ app.listen(port, () => {
  * @param {*} file File name
  */
 function fsend(res, file) {
-  var dirname = getDirname();
+  const dirname = getDirname();
 
   res.sendFile(path.join(dirname, '/' + file));
 }
@@ -45,6 +80,6 @@ function fsend(res, file) {
  * @returns The directory path of the server
  */
 function getDirname() {
-  var filename = url.fileURLToPath(import.meta.url);
+  const filename = url.fileURLToPath(import.meta.url);
   return path.dirname(filename);
 }
