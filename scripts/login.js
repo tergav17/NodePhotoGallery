6bcc5d7b25ad9e081ba797fb0e8529adb8841a11
imundy <imundy@iastate.edu>
2022-11-16 22:29:25 -0600

    javascript for login?

---
+++ scripts/login.js
@@ -0,0 +1,25 @@
+function login(){
+    //let bool = validateLogin();
+    // if (bool) window.location.href = "index.html";
+
+    document.getElementById("uname").innerText = "Invalid";
+    
+	/*let userNameInput = document.getElementById("uName");
+    let passwordInput  = document.getElementById("uPass");	
+	
+	if (bool) fetch("/login", {
+		method: "POST",
+		headers: {'Content-Type': 'application/json'}, 
+		body: "{ \"username\": \"" + userNameInput.value + "\", \"password\": \"" + passwordInput.value + "\" }"
+	}).then(response => response.json()).then(data => {
+		if (data.valid == "true") {
+            document.cookie = data.token;
+            window.location.href = "index.html"
+
+            // storeUser(userNameInput); //need this function from index.js
+		} else {
+            document.getElementById("uname").innerText = "Invalid";
+			document.getElementById("upass").innerText = "";
+		}
+	});*/
+}
