ecaca7f013f82e3dc646f051c0a7fae818c419f9
tergav17 <gctersteeg@gmail.com>
2022-11-17 12:17:22 -0600

    Search is now functional on the frontend

---
+++ scripts/search.js
@@ -0,0 +1,28 @@
+function search(){
+
+	let searchQuery = document.getElementById("searchPrompt");
+
+	// Send a post request to the search endpoint
+	fetch("/post/search", {
+		method: "POST",
+		headers: {'Content-Type': 'application/json'}, 
+        // Include the query
+		body: "{ \"query\": \"" + searchQuery.value + "\"}"
+
+    // Handle response here
+	}).then(response => response.json()).then(data => {
+
+        const results = document.getElementById("searchResultContent");
+
+        results.innerHTML = '';
+
+		for (const image of data) {
+            console.log(image.imageName);
+
+            var elem = document.createElement("img");
+            elem.setAttribute("src", "/u/" + image.imageName);
+            results.appendChild(elem);
+        }
+	});
+
+}
